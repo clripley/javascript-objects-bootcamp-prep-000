82ee387ced954ecab1719a7ed693214e84a64dac
Christopher Ripley <clripley@gmail.com>
2020-07-05 22:53:09 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,1 @@
-var playlist = { Slowdive: "Alison" }
-function updatePlaylist(playlist, Slowdive, Alison){
-  playlist ['My Bloody Valentine'] = 'Sometimes'
-  playlist ["Phil Ochs"] = "here\'s to the state of mississippi
-}
 
