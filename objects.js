a4022c102b104403d89e461263b1865b452f4edd
Christopher Ripley <clripley@gmail.com>
2020-07-05 22:36:35 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,1 +1,6 @@
 var playlist = { Boardsofcanada: "sixtyniner" }
+function updatePlaylist(playlist,Boardsofcanada,sixtyniner){
+  var playlist = { 
+    Boardsofcanada: 'sixtyniner'
+    Boardsofcanada: 'oneveryimportantthought'}
+}
